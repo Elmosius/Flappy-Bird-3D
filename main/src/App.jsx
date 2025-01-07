3059e9b6daf491f0f90e4b24bfa0fc4a65bb8672
Elmosius <2272008@maranatha.ac.id>
2025-01-07 10:43:10 +0700

    coba-coba

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from "@react-three/fiber";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="canvas-container" style={{ width: "100vw", height: "100vh" }}>
+      <Canvas
+        style={{
+          background: "lightblue",
+        }}
+      >
+        <mesh>
+          {/* <sphereGeometry args={[1, 8, 8]} /> */}\
+          <boxGeometry args={[1, 1, 1]} />
+          <meshPhongMaterial color={0x00bfff} />
+          <ambientLight />
+          <directionalLight position={[0, 0, 5]} intensity={1} />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
-
-export default App
