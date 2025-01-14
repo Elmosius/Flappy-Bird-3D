9857b1236018c7920f3ccca4b20aaa6d0ecee45d
Josephine Alvina L <josephinealvinaluwia@gmail.com>
2025-01-15 06:50:03 +0700

    add: menu star, credits & game over

--- src/index-V2.js
+++ src/index-V2.js
@@ -1,17 +1,31 @@
-import * as CANNON from "cannon-es";
-import * as THREE from "three";
-import Controls from "./components/controls.js";
-import Lights from "./components/lights.js";
-import Floor from "./components/floor.js";
-import SkyBox from "./components/SkyBox.js";
-import Pipe from "./components/Pipe.js";
-import Bird from "./components/Bird.js";
-import KeyboardHelper from "./components/Keyboard.js";
+import * as CANNON from 'cannon-es';
+import * as THREE from 'three';
+import Controls from './components/controls.js';
+import Lights from './components/lights.js';
+import Floor from './components/floor.js';
+import SkyBox from './components/SkyBox.js';
+import Pipe from './components/Pipe.js';
+import Bird from './components/Bird.js';
+import KeyboardHelper from './components/Keyboard.js';
+
+// Cek apakah halaman telah diakses sebelumnya
+if (!localStorage.getItem('fromOpening')) {
+	// Jika tidak, kembalikan ke halaman openning.html
+	window.location.href = 'opening.html';
+} else {
+	// Jika ya, hapus status untuk mencegah reload kembali ke openning.html
+	localStorage.removeItem('fromOpening');
+}
 
 //* SETUP
 const scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0xa0a0a0, 20, 100);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -30,41 +44,49 @@ const keyboard = new KeyboardHelper();
 
 // PIPA
 const textureLoader = new THREE.TextureLoader();
-const barkColorTexture = textureLoader.load("./assets/textures/Bark014_1K-PNG_Color.png");
-const barkAOTexure = textureLoader.load("./assets/textures/Bark014_1K-PNG_AmbientOcclusion.png");
-const barkRoughnessTexture = textureLoader.load("./assets/textures/Bark014_1K-PNG_Roughness.png");
-const barkNormalTexture = textureLoader.load("./assets/textures/Bark014_1K-PNG_NormalGL.png");
+const barkColorTexture = textureLoader.load(
+	'./assets/textures/Bark014_1K-PNG_Color.png'
+);
+const barkAOTexure = textureLoader.load(
+	'./assets/textures/Bark014_1K-PNG_AmbientOcclusion.png'
+);
+const barkRoughnessTexture = textureLoader.load(
+	'./assets/textures/Bark014_1K-PNG_Roughness.png'
+);
+const barkNormalTexture = textureLoader.load(
+	'./assets/textures/Bark014_1K-PNG_NormalGL.png'
+);
 const barkMaterial = new THREE.MeshStandardMaterial({
-  map: barkColorTexture,
-  aoMap: barkAOTexure,
-  roughnessMap: barkRoughnessTexture,
-  normalMap: barkNormalTexture,
-  roughness: 1.0,
-  metalness: 0.0,
+	map: barkColorTexture,
+	aoMap: barkAOTexure,
+	roughnessMap: barkRoughnessTexture,
+	normalMap: barkNormalTexture,
+	roughness: 1.0,
+	metalness: 0.0,
 });
 for (let i = 0; i < 5; i++) {
-  new Pipe(scene, barkMaterial, world, i * 20 + 10, 0, 20, 10);
+	new Pipe(scene, barkMaterial, world, i * 20 + 10, 0, 20, 10);
 }
 // end of PIPA
 const bird = new Bird(scene, camera, world);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 function animate() {
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  world.step(1 / 60);
+	world.step(1 / 60);
 
-  if (keyboard.keys[" "]) {
-    bird.jump();
-  }
+	if (keyboard.keys[' ']) {
+		bird.jump();
+	}
 
-  bird.update();
-  renderer.render(scene, camera);
+	bird.update();
+	renderer.render(scene, camera);
 }
 animate();
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 });
